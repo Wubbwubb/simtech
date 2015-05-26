e14c5d1982a5337de00a812991a6eb406366653b
Wubbwubb <schmittmax91@gmail.com>
2015-05-27 00:02:46 +0200

    Musterlösung für Übung 2 hinzugefügt

---
+++ SimTech Übungen/src/muster/uebung02/CounterCollection.java
@@ -0,0 +1,76 @@
+package muster.uebung02;
+
+import muster.uebung01.SimParam;
+import muster.uebung01.SystemState;
+
+/**
+ *
+ * Exercise 2 Simulationstechnink SS2015
+ *
+ * CounterCollection class Consists of Counters and Histograms
+ * 
+ * @author David Hock, Alexander Klein, Dirk Staehle
+ * @version 1.1.0
+ * @since 2005-11-06
+ */
+public class CounterCollection {
+	/**
+	 * Counter - waiting time per customer
+	 */
+	public static TimeIndependentCounter counter_waiting_time = new TimeIndependentCounter("waiting time per customer");
+	/**
+	 * Histogram - waiting time per customer
+	 */
+	public static TimeIndependentHistogram hist_waiting_time = new TimeIndependentHistogram(
+			"waiting time per customer", "wtc.txt", 0, SimParam.buffersize * 1000, 10);
+	/**
+	 * Counter - queue length
+	 */
+	public static TimeDependentCounter counter_queuelength = new TimeDependentCounter("queue length");
+	/**
+	 * Histogram - queue length
+	 */
+	public static TimeDependentHistogram hist_queuelength = new TimeDependentHistogram("queue length", "ql.txt", 0,
+			SimParam.buffersize, SimParam.buffersize + 1);
+
+	/**
+	 * Function calls the reset method of all counters and histograms
+	 */
+	public static void reset() {
+		counter_waiting_time.reset();
+		hist_waiting_time.reset();
+		counter_queuelength.reset();
+		hist_queuelength.reset();
+	}
+
+	/**
+	 * Function calls the report method of all counters and histograms
+	 */
+	public static void report() {
+		counter_waiting_time.report();
+		counter_queuelength.report();
+		hist_waiting_time.report();
+		hist_queuelength.report();
+
+	}
+
+	/**
+	 * Count the waiting time of the given packet
+	 * 
+	 * @param p
+	 *            the given packet
+	 */
+	public static void count_packet(Packet p) {
+		counter_waiting_time.count(p.getWaitingTime());
+		hist_waiting_time.count(p.getWaitingTime());
+	}
+
+	/**
+	 * Count the queue length
+	 */
+	public static void count_system_state() {
+		counter_queuelength.count(SystemState.getPacketsInBuffer());
+		hist_queuelength.count(SystemState.getPacketsInBuffer());
+	}
+
+}
---
+++ SimTech Übungen/src/muster/uebung02/FiniteQueue.java
@@ -0,0 +1,95 @@
+package muster.uebung02;
+
+import java.util.Iterator;
+import java.util.Vector;
+
+/**
+ *
+ * Exercise 2 Simulationstechnink SS2015
+ *
+ * FiniteQueue class Contains a buffer with the packets that are currently in the system as well as additional
+ * information to the packet queue
+ * 
+ * @author David Hock, Alexander Klein, Dirk Staehle
+ * @version 1.1.0
+ * @since 2005-11-06
+ *
+ */
+class FiniteQueue {
+	/**
+	 * Attribute: total number of places in the queue
+	 */
+	private long places;
+	/**
+	 * Attribute: The buffer representing the queue
+	 */
+	private Vector<Packet> buffer;
+
+	/**
+	 * Create an empty queue with size places
+	 * 
+	 * @param size
+	 *            total number of places
+	 */
+	public FiniteQueue(long size) {
+		this.buffer = new Vector<Packet>();
+		this.places = size;
+		// this.occupied = 0;
+	}
+
+	/**
+	 * Add a packet to the queue and return true if successful
+	 * 
+	 * @param p
+	 *            The packet
+	 * @return true - if successful
+	 */
+	public boolean add(Packet p) {
+		if (this.buffer.size() < this.places) {
+			this.buffer.addElement(p);
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Return and remove first element of queue, if queue is empty return null
+	 * 
+	 * @return first element or null
+	 */
+	public Packet remove() {
+		if (!this.buffer.isEmpty()) {
+			return this.buffer.remove(0);
+		}
+		return null;
+	}
+
+	/**
+	 * Returns boolean if queue is empty or not
+	 * 
+	 * @return boolean
+	 */
+	public boolean isEmpty() {
+		return this.buffer.isEmpty();
+	}
+
+	/**
+	 * Returns current queue length
+	 * 
+	 * @return queue length
+	 */
+	public long getQueueLength() {
+		return this.buffer.size();
+	}
+
+	/**
+	 * prints contents of queue
+	 */
+	public void print() {
+		System.out.println(this.buffer.size() + " packets in buffer");
+		Iterator<Packet> pi = this.buffer.iterator();
+		while (pi.hasNext()) {
+			pi.next().print();
+		}
+	}
+}
